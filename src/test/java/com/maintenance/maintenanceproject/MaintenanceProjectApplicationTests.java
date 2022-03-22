0590ef9fb69a642812a2eee9d2975dc051cfe671
Isti16 <isti001116@gmail.com>
2022-03-22 18:45:22 +0100

    first

---
+++ src/test/java/com/maintenance/maintenanceproject/MaintenanceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maintenance.maintenanceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
