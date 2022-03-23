876adc55bf20558e39221b0f404aa8fcf8b1cf36
Isti16 <isti001116@gmail.com>
2022-03-23 21:06:05 +0100

    new batch

--- src/main/java/com/maintenance/maintenanceproject/controller/DeviceController.java
+++ src/main/java/com/maintenance/maintenanceproject/controller/DeviceController.java
@@ -5,16 +5,22 @@ import java.util.List;
 import com.maintenance.maintenanceproject.model.Device;
 import com.maintenance.maintenanceproject.service.DeviceService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class DeviceController {
     
     private DeviceService deviceService;
 
-    @GetMapping()
+    @Autowired
+    public DeviceController(DeviceService deviceService) {
+        this.deviceService = deviceService;
+    }
+
+    @GetMapping("/devices")
     public List<Device> getDevices() {
-        
+        return deviceService.getDevices();
     }
 }
--- src/main/java/com/maintenance/maintenanceproject/service/DeviceService.java
+++ src/main/java/com/maintenance/maintenanceproject/service/DeviceService.java
@@ -1,5 +1,19 @@
 package com.maintenance.maintenanceproject.service;
 
+import java.util.List;
+
+import com.maintenance.maintenanceproject.model.Device;
+
+
 public class DeviceService {
-    
+    public List<Device> getDevices() {
+        return List.of(
+            new Device(
+                id:1,
+                name:"Demo",
+                location:"place",
+                category:"category"
+            )
+        );
+    }
 }
